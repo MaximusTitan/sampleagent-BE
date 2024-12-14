081a4b229942c43d911a4cb9d837d23096e26b10
Shrideep_Tamboli <27.tshrideep@gmail.com>
2024-12-14 16:28:10 +0530

    allow all origin

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[base_url],  # Or specify the frontend domain: ["http://localhost:3000"]
+    allow_origins=["*"],  # Or specify the frontend domain: ["http://localhost:3000"]
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
